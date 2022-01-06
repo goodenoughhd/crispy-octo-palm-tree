128d46bae168652d15c08a78d2a7dbafd438b920
goodenoughhd <wtfidontknowwtf@gmail.com>
2022-01-06 12:44:31 +0300

    06.01.22_12:44

--- Flask/First Flask/app.py
+++ Flask/First Flask/app.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+
+
+import os
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
+
+# Dropzone settings
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+
+# Uploads settings
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
