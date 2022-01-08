3d9165b669f37af2ad466b3e9c66fae106fbc06a
goodenoughhd <wtfidontknowwtf@gmail.com>
2022-01-08 20:56:15 +0300

    08.01.22_20:56

--- Flask/First Flask/app.py
+++ Flask/First Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 
@@ -8,11 +8,14 @@ import os
 app = Flask(__name__)
 dropzone = Dropzone(app)
 
+app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
+
 # Dropzone settings
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
 app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+
 
 # Uploads settings
 app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
@@ -21,15 +24,49 @@ photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
+    # set session for image results
+  if "file_urls" not in session:
+    session['file_urls'] = []
+  # list to hold our uploaded image urls
+    file_urls = session['file_urls']
+
+  if request.method == 'POST':
+    file_obj = request.files
+    for f in file_obj:
+      file = request.files.get(f)
+            
+      # save the file with to our photos folder
+      filename = photos.save(file, name=file.filename)            
+            
+      # append image urls
+      file_urls.append(photos.url(filename))
+
+    session['file_urls'] = file_urls    
+    return "uploading..."
+
+  # return dropzone template on GET request     
   return render_template('index.html')
+  print(render_template)
 
 @app.route('/results')
 def results():
-    return render_template('results.html')
+
+  # redirect to home if no images to display
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('index'))
+        
+    # set the file_urls and remove the session variable
+    file_urls = session['file_urls']
+    session.pop('file_urls', None)
+
+    return render_template('results.html', file_urls=file_urls)
 
 if __name__ == '__main__':
     app.run()
+
+
 
 
